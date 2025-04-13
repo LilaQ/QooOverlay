e119769f75b345b5bc82096f98f5e0ee28746f9f
LilaQ <teh.lilaq@gmail.com>
2025-04-14 01:21:46 +0200

    - added notification template

--- QooOverlay/main.cpp
+++ QooOverlay/main.cpp
@@ -3,7 +3,7 @@
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     OverlayManager manager;
-    manager.AddOverlay(L"Qoo PDF 1", L"file:///C:/Users/LilaQ/Desktop/Projects/QooOverlay/QooOverlay/assets/pdf_overlay1.html", MOD_CONTROL | MOD_SHIFT, 'P');
+    manager.AddOverlay(L"Qoo PDF 1", L"file:///C:/Users/LilaQ/Desktop/Projects/QooOverlay/QooOverlay/assets/notification.html", MOD_CONTROL | MOD_SHIFT, 'P');
     manager.AddOverlay(L"Qoo PDF 2", L"file:///C:/Users/LilaQ/Desktop/Projects/QooOverlay/QooOverlay/assets/pdf_overlay2.html", MOD_CONTROL | MOD_SHIFT, 'O');
     manager.Run();
     return 0;
